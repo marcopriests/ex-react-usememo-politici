56a0b18cf09b72bee2dc01f3c04ff823d546b237
marcopriests <prtmrc98@gmail.com>
2025-07-22 10:56:34 +0200

    [UPD] milestone 1 completata. Immagini non disponibili, le ho rimosse

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react"
+
 function App() {
+  const [politicians, setPoliticians] = useState([]);
+
+  function getData() {
+    fetch(`http://localhost:3333/politicians`)
+      .then(res => res.json())
+      .then(data => setPoliticians(data))
+      .catch(err => console.error(err))
+  };
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   return (
     <>
+      <h1>Lista politici</h1>
+      <div className="card-container">
+        {politicians.map((p, i) => {
+          return (
+            <div className="card" key={i}>
+              <div className="card-info">
+                <h4>{p.name}</h4>
+                <p><strong>{p.position}</strong></p>
+                <p>{p.biography}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
